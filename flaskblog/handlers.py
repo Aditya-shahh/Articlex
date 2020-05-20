e29343e9cb496d745ea535f256a2431a1d685f75
Aditya-shahh <adishah3103@gmail.com>
2020-05-20 22:43:59 +0530

    init

---
+++ flaskblog/handlers.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from flaskblog import app
+
+@app.errorhandler(404)
+def error_404(error):
+	return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def error_403(error):
+	return render_template('403.html'), 403
+	
+@app.errorhandler(500)
+def error_500(error):
+	return render_template('500.html'), 500
+	
